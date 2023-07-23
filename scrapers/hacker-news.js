5f6850957f886cdda4258da42d268d6a1e24a57f
coding-to-music <connors.tom@gmail.com>
2023-07-22 21:33:07 -0500

    Build

--- scrapers/hacker-news.js
+++ scrapers/hacker-news.js
@@ -1,23 +1,64 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-puppeteer.launch({ headless: true, args: ['--user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"] }).then(async)
+const scrapeHackerNews = async () => {
+  //   const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
+  await page.goto("https://news.ycombinator.com/", {
+    waitUntil: "domcontentloaded",
+  });
 
-const page = await puppeteer.Browser.newPage();
-await page.goto("https://news.ycombinator.com/");
-await page.waitForSelector('body');
+  const topPosts = await page.evaluate(() => {
+    const posts = [];
+    const postElements = document.querySelectorAll(".athing");
 
+    postElements.forEach((element) => {
+      const titleElement = element.querySelector(".storylink");
+      const title = titleElement ? titleElement.textContent : "";
 
-var rposts = await page.evaluate(() -> {
+      const url = titleElement ? titleElement.href : "";
 
-});
+      const authorElement = element.nextElementSibling.querySelector(".hnuser");
+      const author = authorElement ? authorElement.textContent : "";
 
-console.log(rposts);
-await puppeteer.Browser.close();
-]
-}).catch(function(error)) {
-    console.error(error);
-});
+      posts.push({ title, url, author });
+    });
+
+    return posts;
+  });
+
+  await browser.close();
+  return topPosts;
+};
+
+scrapeHackerNews()
+  .then((topPosts) => {
+    console.log("Top posts on Hacker News:");
+    console.log(topPosts);
+  })
+  .catch((err) => {
+    console.error("Error while scraping:", err);
+  });
+
+// const puppeteer = require('puppeteer');
+
+// puppeteer.launch({ headless: true, args: ['--user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"] }).then(async)
+
+// const page = await puppeteer.Browser.newPage();
+// await page.goto("https://news.ycombinator.com/");
+// await page.waitForSelector('body');
+
+// var rposts = await page.evaluate(() -> {
+
+// });
+
+// console.log(rposts);
+// await puppeteer.Browser.close();
+// ]
+// }).catch(function(error)) {
+//     console.error(error);
+// });
 
 // postItems = [];
 
