3b5998f6a1eef107e4d66571e79b8fbe35d475b2
coding-to-music <connors.tom@gmail.com>
2023-07-26 16:02:16 -0500

    Build

--- scrapers/hacker-news.js
+++ scrapers/hacker-news.js
@@ -66,6 +66,7 @@ scrapeHackerNews()
   });
 
 // const puppeteer = require('puppeteer');
+//
 
 // headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 
